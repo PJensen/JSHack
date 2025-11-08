10d6e3eee932f165ab3de1f4db637937dbb726e0
Pete Jensen <jensen.petej@gmail.com>
2025-11-08 15:15:06 -0500

    spawner changes

--- src/rules/archetypes/Spawner.js
+++ src/rules/archetypes/Spawner.js
@@ -3,12 +3,15 @@ import { Position } from "../components/Position.js";
 import { NamedIdentity } from "../components/NamedIdentity.js";
 import { Vitality } from "../components/Vitality.js";
 import { MonsterSpawner } from "../components/MonsterSpawner.js";
+import { BoundingCircle } from "../components/BoundingCircle.js";
 
 export const Spawner = defineArchetype(
   "Spawner",
   [Position, (p) => ({ x: p.x ?? 0, y: p.y ?? 0 })],
   [NamedIdentity, (p) => ({ name: p.name ?? "Monster Spawner", identity: p.identity ?? "spawner" })],
   [Vitality, (p) => ({ maxHp: p.maxHp ?? 100, hp: p.hp ?? (p.maxHp ?? 100) })],
+  // Give spawner a physical footprint for movement sweeps
+  [BoundingCircle, (p) => ({ radius: Math.max(0, p.colliderRadius ?? 0.7) })],
   [MonsterSpawner, (p) => ({
     maxConcurrent: p.maxConcurrent ?? 3,
     cooldownTicks: p.cooldownTicks ?? 10,
