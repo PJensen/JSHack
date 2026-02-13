3594e2798667bc176e390eae6c0c43a64a2680b0
Pete Jensen <pjensen@diemos>
2026-02-12 20:15:37 -0500

    breakout archetypes to single files

--- src/rules/archetypes/index.js
+++ src/rules/archetypes/index.js
@@ -1,6 +1,8 @@
 // Barrel exports for archetypes used by the headless smoke test and elsewhere
 export { Creature, Human, Monster, Other, HumanoidBase } from './Creatures.js';
 export { Door } from './Door.js';
+export { Chest } from './Chest.js';
+export { Sign } from './Sign.js';
 export { FloorTile, WallTile } from './Tiles.js';
 export { PlayerArchetype, createPlayer } from './Player.js';
 export { Spawner } from './Spawner.js';
