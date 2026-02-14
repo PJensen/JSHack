b64f2f2d74c9468a94a0e28bc6b53a41c8a1645e
Pete Jensen <jensen.petej@gmail.com>
2026-02-13 21:46:30 -0500

    chests no longer have a collider, small UX tweaks

--- src/rules/archetypes/Chest.js
+++ src/rules/archetypes/Chest.js
@@ -1,7 +1,5 @@
 import { defineArchetype } from "../../lib/ecs-js/archetype.js";
 import { Position } from "../components/Position.js";
-import { Collider } from "../components/Collider.js";
-import { Interactable } from "../components/Interactable.js";
 import { Material } from "../components/Material.js";
 import { NamedIdentity } from "../components/NamedIdentity.js";
 import { Inventory } from "../components/Inventory.js";
@@ -10,8 +8,6 @@ export const Chest = defineArchetype(
   "Chest",
   [Position, (p) => ({ x: p.x, y: p.y })],
   [NamedIdentity, { name: "Chest", identity: "chest" }],
-  [Collider, { solid: true, blocksSight: false }],
-  [Interactable, { action: "openChest", params: { lootTable: "basic" } }],
   [Material, { kind: "iron" }],
   [Inventory, { items: [], capacity: 20, weightLimit: null }]
 );
