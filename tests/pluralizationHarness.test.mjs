fb0cd8ef7169300f03e984509527a62d6e55a675
Pete Jensen <jensen.petej@gmail.com>
2026-02-26 11:20:00 -0500

    finalizing test harness

--- tests/pluralizationHarness.test.mjs
+++ tests/pluralizationHarness.test.mjs
@@ -1,4 +1,5 @@
 import { assertEquals } from "jsr:@std/assert";
+import "./00_testHarness.test.mjs";
 
 Deno.test("test harness installs pluralization string extensions", () => {
   assertEquals("wolf".pluralize(2), "wolves");
