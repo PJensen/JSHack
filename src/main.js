a4ee10c74ba0265ae2bd0dd09a0faed6ee211b4e
Pete Jensen <jensen.petej@gmail.com>
2026-03-26 22:12:33 -0400

    PROC NODE: leave meaningful text

--- src/main.js
+++ src/main.js
@@ -4154,7 +4154,7 @@ function drawFacingDot(ctx, entity) {
 
   ctx.save();
   ctx.globalCompositeOperation = 'source-over';
-  ctx.fillStyle = 'rgba(70,130,190,0.97)';
+  ctx.fillStyle = 'rgba(220,195,95,0.98)';
   ctx.beginPath();
   ctx.arc(dotX, dotY, 0.055, 0, Math.PI * 2);
   ctx.fill();
