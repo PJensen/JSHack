efbfeb286ffee85fddea4bea1a15a32d0f11f766
Pete Jensen <jensen.petej@gmail.com>
2026-04-30 00:00:00 -0400

    ADDED: content authoring barrel

--- tests/00_testHarness.test.mjs
+++ tests/00_testHarness.test.mjs
@@ -1,3 +1,11 @@
 import { installPluralizationExtensions } from "../src/shared/utils/pluralization.js";
+import "../src/content/items/potionOfRadiance.js";
+import "../src/content/items/sunVessel.js";
+import "../src/content/items/dawnbreaker.js";
+import "../src/content/items/sunsword.js";
+import "../src/content/items/fishingRod.js";
+import "../src/content/items/lodbrokSerpentBoundBreeches.js";
+import { installContent } from "../src/content/install.js";
 
 installPluralizationExtensions();
+installContent();
