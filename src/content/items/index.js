cd4452a6be74cfe8e2f2ad4b7f1c7fbab35fc0a2
Pete Jensen <pjensen@diemos>
2026-04-30 00:00:00 -0400

    ADDED: content authoring barrel

---
+++ src/content/items/index.js
@@ -0,0 +1,9 @@
+// Barrel import: loads all content-DSL item definitions.
+// Importing this file ensures all items are registered in the content registry.
+// Call installContent() afterward to merge into ITEM_CATALOG.
+export * from "./potionOfRadiance.js";
+export * from "./sunVessel.js";
+export * from "./dawnbreaker.js";
+export * from "./sunsword.js";
+export * from "./fishingRod.js";
+export * from "./lodbrokSerpentBoundBreeches.js";
