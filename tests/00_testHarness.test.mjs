a6e9f82a246d646042216ed5c9a9e8e050170da2
Pete Jensen <jensen.petej@gmail.com>
2026-05-03 00:00:00 -0400

    ARHC: continued monsters cutout. (pt2)

--- tests/00_testHarness.test.mjs
+++ tests/00_testHarness.test.mjs
@@ -1,5 +1,6 @@
 import { installPluralizationExtensions } from "../src/shared/utils/pluralization.js";
 import "../src/content/items/index.js";
+import "../src/content/monsters/index.js";
 import { installContent } from "../src/content/install.js";
 
 installPluralizationExtensions();
--- tests/helpers/installContentCatalog.mjs
+++ tests/helpers/installContentCatalog.mjs
@@ -1,5 +1,5 @@
 import "../../src/content/items/index.js";
-import "../../src/content/monsters/barrowWight.js";
+import "../../src/content/monsters/index.js";
 import { installContent } from "../../src/content/install.js";
 
 installContent();
