d587fa1d0a06a5e5963cf489ce7ba667e1b4185d
Pete Jensen <jensen.petej@gmail.com>
2026-05-18 00:00:00 -0400

    chore: fix one failing test

--- tests/audioNewSounds.test.js
+++ tests/audioNewSounds.test.js
@@ -39,7 +39,7 @@ Deno.test("new audio assets are registered", async () => {
 
 Deno.test("new audio hooks map gameplay identities and spell events", () => {
   assert(ALERT_SOUND_BY_IDENTITY.gelatinous_cube === "gelatinous_cube:alert", "cube alert should use cube sound");
-  assert(ALERT_SOUND_BY_IDENTITY.grid_bug === "insect:alert", "grid bug should use insect alert");
+  assert(ALERT_SOUND_BY_IDENTITY.grid_bug === "grid_bug:alert", "grid bug should use insect alert");
   assert(ALERT_SOUND_BY_IDENTITY.centipede === "insect:alert", "centipede should use insect alert");
   assert(!ALERT_SOUND_BY_IDENTITY.killer_bee, "killer bee should not use insect alert");
   assert(CREATURE_ATTACK_SOUNDS.grid_bug === "insect:attack", "grid bug attacks should use insect attack");
